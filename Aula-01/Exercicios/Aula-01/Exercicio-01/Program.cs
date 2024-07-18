21905c1e9e320b80f34dd59cd02c47da6269f2a9
GabrielKameoka <gabrielkameoka@gmail.com>
2024-07-18 10:48:29 -0300

    Atualizando pastas de exercícios

---
+++ Aula-01/Exercicios/Aula-01/Exercicio-01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio {
+    class Program {
+        static void Main(string[] args) {
+            
+            int A = int.Parse (Console.ReadLine());
+            int B = int.Parse (Console.ReadLine());
+
+            if (A % B == 0 || B % A == 0){ 
+                System.Console.WriteLine("São múltiplos");
+            } else {
+                System.Console.WriteLine("Não são múltiplos");
+            }
+            
+        }
+    }
+}
--- Aula-01/Exercicios/Aula-01/Exercicio-01/obj/Debug/net8.0/Exercicio-01.AssemblyInfo.cs
+++ Aula-01/Exercicios/Aula-01/Exercicio-01/obj/Debug/net8.0/Exercicio-01.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-04")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio-01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc147099fd40923061ca98f28606a78d308b452")]
-[assembly: System.Reflection.AssemblyProductAttribute("Projeto-04")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Projeto-04")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exercicio-01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exercicio-01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
